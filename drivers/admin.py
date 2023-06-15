1044390ecabe258396d333e3e202017d31d0d26d
git-anik-3 <anik.devxhub@gmail.com>
2023-06-16 03:06:20 +0600

    requirements.txt

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from drivers.models import *
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Company)
+admin.site.register(EmployeeFile)
+admin.site.register(OwnerEmployeeNotification)
+admin.site.register(MedicalTestCenter)
+admin.site.register(DotDrugTest)
+admin.site.register(MedicalTestForm)
